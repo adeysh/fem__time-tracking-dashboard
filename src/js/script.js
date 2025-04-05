3efbf5c39b1f97cc6dd4e3c6e070ff71dd36aebd
Adesh Katiya <adeshkatiya27@gmail.com>
2025-04-06 02:01:49 +0530

    --> Update class names for consistency. --> Improve data fetching logic for dynamic updation from json file externally

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,27 @@
+// Getting all the option buttons and card items
 const buttons = document.querySelectorAll(".card__option");
-const cards = document.querySelectorAll(".card_item");
+const cards = document.querySelectorAll(".card-item");
 
+// Fetching data from json
+async function fetchTimeData() {
+    try {
+        const response = await fetch(
+            "/data.json",
+        );
+        if (!response.ok) {
+            throw new Error(`HTTP error: ${response.status}`);
+        }
+        const data = await response.json();
+
+        return data
+    } catch (error) {
+        console.error(`Could not get data: ${error}`);
+    }
+}
+
+// Making objects for every card element that will be used for updating
 let cardElements = {};
+
 cards.forEach(card => {
     cardElements[card.id] = {
         currentHours: card.querySelector(".card__current-hours"),
@@ -10,90 +30,35 @@ cards.forEach(card => {
     };
 });
 
-buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        switch (button.id) {
-            case "daily":
+const cardList = ["work", "play", "study", "exercise", "social", "self-care"];
 
-                cardElements["card-work"].currentHours.textContent = "5hrs";
-                cardElements["card-work"].comparisonTitle.textContent = "Yesterday";
-                cardElements["card-work"].comparisonPreviousTime.textContent = "7hrs";
+// Changing the timeframe according to the button click and updating all the cards
+function changeTimeFrameAndData(timedata, timeframe) {
+    const titleMap = {
+        daily: "Yesterday",
+        weekly: "Last Week",
+        monthly: "Last Month",
+    }
 
-                cardElements["card-play"].currentHours.textContent = "1hrs";
-                cardElements["card-play"].comparisonTitle.textContent = "Yesterday";
-                cardElements["card-play"].comparisonPreviousTime.textContent = "2hrs";
+    cardList.forEach((item, index) => {
+        const cardData = timedata[index].timeframes[timeframe];
+        cardElements[`card-${item}`].currentHours.textContent = `${cardData.current}hrs`
+        cardElements[`card-${item}`].comparisonTitle.textContent = titleMap[timeframe];
+        cardElements[`card-${item}`].comparisonPreviousTime.textContent = `${cardData.previous}hrs`;
+    });
+}
 
-                cardElements["card-study"].currentHours.textContent = "0hrs";
-                cardElements["card-study"].comparisonTitle.textContent = "Yesterday";
-                cardElements["card-study"].comparisonPreviousTime.textContent = "1hrs";
+// Adding an event listener for every button
+function changeTime(timeData) {
+    buttons.forEach(button => button.addEventListener("click", () => changeTimeFrameAndData(timeData, button.id)));
+}
 
-                cardElements["card-exercise"].currentHours.textContent = "1hrs";
-                cardElements["card-exercise"].comparisonTitle.textContent = "Yesterday";
-                cardElements["card-exercise"].comparisonPreviousTime.textContent = "1hrs";
+// Getting time data and passing it for updating time
+async function dashboardDataUpdate() {
+    const timeData = await fetchTimeData();
+    changeTime(timeData);
+}
 
-                cardElements["card-social"].currentHours.textContent = "1hrs";
-                cardElements["card-social"].comparisonTitle.textContent = "Yesterday";
-                cardElements["card-social"].comparisonPreviousTime.textContent = "3hrs";
-
-                cardElements["card-self-care"].currentHours.textContent = "0hrs";
-                cardElements["card-self-care"].comparisonTitle.textContent = "Yesterday";
-                cardElements["card-self-care"].comparisonPreviousTime.textContent = "1hrs";
-                break;
-            case "weekly":
-                console.log("weekly");
-                cardElements["card-work"].currentHours.textContent = "32hrs";
-                cardElements["card-work"].comparisonTitle.textContent = "Last Week";
-                cardElements["card-work"].comparisonPreviousTime.textContent = "36hrs";
-
-                cardElements["card-play"].currentHours.textContent = "10hrs";
-                cardElements["card-play"].comparisonTitle.textContent = "Last Week";
-                cardElements["card-play"].comparisonPreviousTime.textContent = "8hrs";
-
-                cardElements["card-study"].currentHours.textContent = "4hrs";
-                cardElements["card-study"].comparisonTitle.textContent = "Last Week";
-                cardElements["card-study"].comparisonPreviousTime.textContent = "7hrs";
-
-                cardElements["card-exercise"].currentHours.textContent = "4hrs";
-                cardElements["card-exercise"].comparisonTitle.textContent = "Last Week";
-                cardElements["card-exercise"].comparisonPreviousTime.textContent = "5hrs";
-
-                cardElements["card-social"].currentHours.textContent = "5hrs";
-                cardElements["card-social"].comparisonTitle.textContent = "Last Week";
-                cardElements["card-social"].comparisonPreviousTime.textContent = "10hrs";
-
-                cardElements["card-self-care"].currentHours.textContent = "2hrs";
-                cardElements["card-self-care"].comparisonTitle.textContent = "Last Week";
-                cardElements["card-self-care"].comparisonPreviousTime.textContent = "2hrs";
-                break;
-            case "monthly":
-                console.log("monthly");
-                cardElements["card-work"].currentHours.textContent = "103hrs";
-                cardElements["card-work"].comparisonTitle.textContent = "Last Month";
-                cardElements["card-work"].comparisonPreviousTime.textContent = "128hrs";
-
-                cardElements["card-play"].currentHours.textContent = "23hrs";
-                cardElements["card-play"].comparisonTitle.textContent = "Last Month";
-                cardElements["card-play"].comparisonPreviousTime.textContent = "29hrs";
-
-                cardElements["card-study"].currentHours.textContent = "13hrs";
-                cardElements["card-study"].comparisonTitle.textContent = "Last Month";
-                cardElements["card-study"].comparisonPreviousTime.textContent = "19hrs";
-
-                cardElements["card-exercise"].currentHours.textContent = "11hrs";
-                cardElements["card-exercise"].comparisonTitle.textContent = "Last Month";
-                cardElements["card-exercise"].comparisonPreviousTime.textContent = "18hrs";
-
-                cardElements["card-social"].currentHours.textContent = "21hrs";
-                cardElements["card-social"].comparisonTitle.textContent = "Last Month";
-                cardElements["card-social"].comparisonPreviousTime.textContent = "23hrs";
-
-                cardElements["card-self-care"].currentHours.textContent = "7hrs";
-                cardElements["card-self-care"].comparisonTitle.textContent = "Last Month";
-                cardElements["card-self-care"].comparisonPreviousTime.textContent = "11hrs";
-                break;
-            default:
-                break;
-        }
-    })
-});
+// Calling the global function
+dashboardDataUpdate();
 
