ee9ad58100169d4f8ff6f66ea08d4757f9d05a03
Adesh Katiya <adeshkatiya27@gmail.com>
2025-04-06 14:57:25 +0530

    --> enhance button interaction and improve data fetching

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,18 @@
 // Getting all the option buttons and card items
+const optionsEl = document.querySelector(".card__options");
 const buttons = document.querySelectorAll(".card__option");
 const cards = document.querySelectorAll(".card-item");
 
 // Fetching data from json
 async function fetchTimeData() {
     try {
-        const response = await fetch(
-            "/data.json",
-        );
+        const response = await fetch("/data.json");
         if (!response.ok) {
             throw new Error(`HTTP error: ${response.status}`);
         }
         const data = await response.json();
 
-        return data
+        return data;
     } catch (error) {
         console.error(`Could not get data: ${error}`);
     }
@@ -22,11 +21,13 @@ async function fetchTimeData() {
 // Making objects for every card element that will be used for updating
 let cardElements = {};
 
-cards.forEach(card => {
+cards.forEach((card) => {
     cardElements[card.id] = {
         currentHours: card.querySelector(".card__current-hours"),
         comparisonTitle: card.querySelector(".card__comparison-title"),
-        comparisonPreviousTime: card.querySelector(".card__comparison-previous-time"),
+        comparisonPreviousTime: card.querySelector(
+            ".card__comparison-previous-time"
+        ),
     };
 });
 
@@ -38,19 +39,32 @@ function changeTimeFrameAndData(timedata, timeframe) {
         daily: "Yesterday",
         weekly: "Last Week",
         monthly: "Last Month",
-    }
+    };
 
     cardList.forEach((item, index) => {
         const cardData = timedata[index].timeframes[timeframe];
-        cardElements[`card-${item}`].currentHours.textContent = `${cardData.current}hrs`
-        cardElements[`card-${item}`].comparisonTitle.textContent = titleMap[timeframe];
-        cardElements[`card-${item}`].comparisonPreviousTime.textContent = `${cardData.previous}hrs`;
+        cardElements[
+            `card-${item}`
+        ].currentHours.textContent = `${cardData.current}hrs`;
+        cardElements[`card-${item}`].comparisonTitle.textContent =
+            titleMap[timeframe];
+        cardElements[
+            `card-${item}`
+        ].comparisonPreviousTime.textContent = `${cardData.previous}hrs`;
     });
 }
 
-// Adding an event listener for every button
+// Listening for click event on any button
 function changeTime(timeData) {
-    buttons.forEach(button => button.addEventListener("click", () => changeTimeFrameAndData(timeData, button.id)));
+    optionsEl.addEventListener("click", (e) => {
+        if (e.target.tagName !== "BUTTON") return;
+
+        buttons.forEach((button) => {
+            button.classList.remove("card__option--active");
+        });
+        e.target.classList.add("card__option--active");
+        changeTimeFrameAndData(timeData, e.target.id);
+    });
 }
 
 // Getting time data and passing it for updating time
@@ -61,4 +75,3 @@ async function dashboardDataUpdate() {
 
 // Calling the global function
 dashboardDataUpdate();
-
